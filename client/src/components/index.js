b5eb8da5548c67a05af1bc4f5656b6ef3c684726
nadpev <nadpev@gmail.com>
2023-04-09 18:56:06 +0300

    first commit

---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import AIPicker from './AIPicker';
+import ColorPicker from './ColorPicker';
+import CustomButton from './CustomButton';
+import FilePicker from './FilePicker';
+import Tab from './Tab';
+export { CustomButton, AIPicker, ColorPicker, FilePicker, Tab };
